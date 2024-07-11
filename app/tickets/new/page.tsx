4a3f8ee43ac9461b9be0033518b0da362f9361fc
josestudionone <105183358+josestudionone@users.noreply.github.com>
2024-07-11 16:26:03 +1000

    Tickets Form

--- app/tickets/new/page.tsx
+++ app/tickets/new/page.tsx
@@ -1,7 +1,9 @@
-"use client";
-
-import TicketForm from "@/components/TicketForm";
+import dynamic from "next/dynamic";
 import React from "react";
+
+const TicketForm = dynamic(() => import("@/components/TicketForm"), {
+  ssr: false,
+});
 
 const NewTicket = () => {
   return <TicketForm />;
